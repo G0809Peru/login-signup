9ca6bae86e0f6f129af6647e3d0ec724e76818f2
G0809Peru <66266391+G0809Peru@users.noreply.github.com>
2021-06-09 22:39:27 +0530

    Add files via upload

---
+++ lib/components/rounded_mobile_number.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/components/text_field_container.dart';
+import 'package:flutter_auth/constants.dart';
+
+class RoundedMobileNumber extends StatelessWidget {
+  final String hintText;
+  final ValueChanged<String> onChanged;
+  const RoundedMobileNumber({
+    Key key,
+    this.hintText,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        cursorColor: kPrimaryColor,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.phone,
+            color: kPrimaryColor,
+          ),
+          hintText: hintText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
